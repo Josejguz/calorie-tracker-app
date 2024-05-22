44fb06f245e61b5657e712ce329ec6871c4141d1
LAPTOP-EPEJ3SK5\jguzm <jguzmansanchez02@gmail.com>
2024-05-21 22:10:00 -0400

    Add Entries to Food Journal

--- src/app.js
+++ src/app.js
@@ -1,19 +1,39 @@
 import Chart from 'chart.js/auto';
 
+
+const breakfastEntries = document.getElementById('breakfast-entries');
+const lunchEntries = document.getElementById('lunch-entries');
+const dinnerEntries = document.getElementById('dinner-entries');
+const snackEntries = document.getElementById('snack-entries');
+
+const addBreakfastEntry = document.getElementById('add-breakfast');
+const addLunchEntry = document.getElementById('add-lunch');
+const addDinnerEntry = document.getElementById('add-dinner');
+const addSnackEntry = document.getElementById('add-snack');
+
+addBreakfastEntry.addEventListener('click', () => {
+    breakfastEntries.innerHTML += `
+        <div class="entry">
+            <input type="text" placeholder="Food" />
+            <input type="number" placeholder="Calories" />
+        </div>
+    `;
+});
+
+
+
 (async function() {
     const data = {
         labels: [
-          'Red',
-          'Blue',
-          'Yellow'
+          'Calories',
+          'Exercise',
         ],
         datasets: [{
-          label: 'My First Dataset',
-          data: [300, 50, 100],
+          label: 'Daily Calorie Intake',
+          data: [300, 50],
           backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)'
+            'rgb(75, 219, 101)',
+            'rgb(64, 137, 220)'
           ],
           hoverOffset: 4
         }]
@@ -24,6 +44,9 @@ import Chart from 'chart.js/auto';
       {
         type: 'doughnut',
         data: data,
+        options: {
+            cutout: '80%',
+        }
       }
     ).resize(272, 272);
   })()
